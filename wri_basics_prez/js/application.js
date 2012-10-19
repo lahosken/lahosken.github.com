4d1c77d8e44a54516b5c9360d63f386f616bf392
Larry Hosken <lhosken@twitter.com>
2012-10-19 10:28:45 -0700

    Better keyboard handling: left arrow should go previous. Better margins

--- wri_basics_prez/js/application.js
+++ wri_basics_prez/js/application.js
@@ -8,19 +8,38 @@
     })
 
     var car = $('#mainCarousel')
-    var next = function(ev){
-      car.carousel("next")
-      ev.preventDefault()
-    }
     var prev = function(ev){
       car.carousel("prev")
       ev.preventDefault()
     }
     car.carousel({interval: false})
-    car.click(next)
+    car.click(function(ev){
+      car.carousel("next")
+      ev.preventDefault()
+    })
     var prevBtn = $('#prevBtn')
     prevBtn.click(prev)
 
-    document.onkeydown = next;
+    document.onkeydown = function(ev){
+      var HANDLERS = { // map keyCode -> direction
+        37: "prev", // left arrow
+        38: "next", // right arrow
+        32: "next", // space
+        72: "prev", // h, for vim nerds
+        76: "next", // l, for vim nerds
+      }
+      if (ev.keyCode && HANDLERS[ev.keyCode]) {
+	  car.carousel(HANDLERS[ev.keyCode])
+	  ev.preventDefault()
+          return
+      }
+      if (ev.altGraphKey || ev.altKey || ev.ctrlKey || ev.metaKey) {
+	  // If user presses Ctrl-C, we don't want the Ctrl or the C
+	  // Don't do anything, don't preventDefault, just
+	  return
+      }
+      car.carousel("next")
+      ev.preventDefault()
+    }
   })
 }(window.jQuery)
