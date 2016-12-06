641a4289b4df7267ff103943ae4cb79db8e777e3
Larry Hosken <lahosken@gmail.com>
2016-12-06 09:06:40 -0800

    spoilery. where we use addresses in button titles, bold the goal in the text description

--- jmas2016/client/locblurbs.js
+++ jmas2016/client/locblurbs.js
@@ -2,12 +2,12 @@
 
 locblurb = {
   bamp: "  <p>Go to the corner of Shattuck and University Avenues. Then play this:\n  <p><b>Testers:</b> The photo is wrong. Sorry. We'll get a better one.\n",
-  brow: "  <p>Make your way to 2150 Allston Way, between Shattuck and Oxford Streets.\n    There find a fence made of metal \u201cslats\u201d with a design twisted into them.\n    The design might remind you of a pair of eyes or perhaps a sound wave.\n    When you\u2019re by the fence, listen to this:\n",
+  brow: "  <p>Make your way to 2150 Allston Way, between Shattuck and Oxford Streets.\n    There, find a fence made of metal \u201cslats\u201d with a design twisted into them.\n    The design might remind you of a pair of eyes or perhaps a sound wave.\n    When you\u2019re by the fence, listen to this:\n",
   clok: "  <p>On the west side of Shattuck Avenue a few paces south of Allston Way,\n    a clock measures time and announces it to all people within view.\n",
-  colg: "  <p>At 2050 Center Street, east of Milvia, is Berkeley City College.\n    Enter the building and walk up some steps to the atrium,\n    where a thin, woven metal fence surrounds a hole below.\n    \n  <p>Pace around this fence as you listen to this track.\n",
+  colg: "  <p>At 2050 Center Street, east of Milvia, is <b>Berkeley City College</b>.\n    Enter the building and walk up some steps to the atrium,\n    where a thin, woven metal fence surrounds a hole below.\n    \n  <p>Pace around this fence as you listen to this track.\n",
   fork: "  <p>There is an island at the intersection of Shattuck Ave and Center St, and in the middle of the island there is a giant, red tuning fork. \n\n  <p>Walk up to it, and press play.\n",
-  pasg: "  <p>Look around you, feel in your pockets.\n    Do you have a penny, or can you find a pebble?\n    Once you have, make your way to the Arts Passage\n    at 2055 Center Street, between Shattuck and Milvia.\n    Make your way into the passage, and press play. \n",
-  poem: "  <p>On the south side of Addison Street near a dangling potted\n    plant attached to a streetlight, there is a poem embedded in\n    the sidewalk, entitled THE WORLD.\n    Once you locate it, play the track below.\n",
+  pasg: "  <p>Look around you, feel in your pockets.\n    Do you have a penny, or can you find a pebble?\n    Once you have, make your way to the <b>Arts Passage</b>\n    at 2055 Center Street, between Shattuck and Milvia.\n    Make your way into the passage, and press play. \n",
+  poem: "  <p>On the south side of Addison Street near a dangling potted\n    plant attached to a streetlight, there is a poem embedded in\n    the sidewalk, entitled <b>THE WORLD</b>.\n    Once you locate it, play the track below.\n",
   vine: "  <p>Go to Trumpetvine Court. It\u2019s a courtyard behind Jupiter Taproom;\n    there are entrance passageways on Allston Way and on Shattuck.\n    When you\u2019re there, listen:\n",
 
 }
